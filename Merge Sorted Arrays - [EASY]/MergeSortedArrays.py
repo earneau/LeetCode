40753104d17819a99feeb4f634f74dd4b4884703
earneau <evan.arn@icloud.com>
2023-11-09 23:31:52 +0100

    Importing solution

--- Merge Sorted Arrays - [EASY]/MergeSortedArrays.py
+++ Merge Sorted Arrays - [EASY]/MergeSortedArrays.py
@@ -7,7 +7,7 @@
 
 #The final sorted array should not be returned by the function, but instead be stored inside the array nums1. To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
 
- 
+
 
 #Example 1:
 
@@ -33,7 +33,7 @@
 #Constraints:
 
 #nums1.length == m + n
-#nums2.length == n
+#nums2.length == n 
 #0 <= m, n <= 200
 #1 <= m + n <= 200
 #-109 <= nums1[i], nums2[j] <= 109
@@ -43,4 +43,25 @@
 
 ############### EXERCISE ###############
 ########################################
+
+def merge(nums1, m, nums2, n):
+    """
+    :type nums1: List[int]
+    :type m: int
+    :type nums2: List[int]
+    :type n: int
+    :rtype: None Do not return anything, modify nums1 in-place instead.
+    """
+    for i in range(n):
+        nums1[m+i] = nums2[i]
+    nums1.sort()
+    return nums1
+    
+nums1 = [1,2,3,0,0,0]
+nums2 = [2,6,7]
+m = 3
+n = 3
+
+result = merge(nums1,m,nums2,n)
+print(result)
 
