4077cbcf78f0bbeca377b4ff2e2dc2e6916b19be
earneau <evan.arn@icloud.com>
2023-11-14 08:49:31 +0100

    Pushing solution O(1)

--- Integer to Roman - [MEDIUM]/integerToRoman.py
+++ Integer to Roman - [MEDIUM]/integerToRoman.py
@@ -46,5 +46,16 @@
 ############### EXERCISE ###############
 ########################################
 
-
-        
+def intToRoman(self, num):
+    """
+    :type num: int
+    :rtype: str
+    """
+    result = ""
+    romanList = [[1000, "M"], [900, "CM"], [500, "D"], [400, "CD"], [100, "C"], [90, "XC"], [50, "L"], [40, "XL"], [10, "X"], [9, "IX"], [5, "V"], [4, "IV"], [1, "I"]]
+    l = len(romanList)
+    for i in range(l):
+        while num >= romanList[i][0]:
+            result += romanList[i][1]
+            num -= romanList[i][0]
+    return result     
