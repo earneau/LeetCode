a2ed4f60447b1a3c4983497881d5365d02a29071
earneau <evan.arn@icloud.com>
2023-11-28 09:44:33 +0100

    Starting binary trees, exercise finished

---
+++ 150 INTERVIEW QUESTIONS/Binary Tree BFS/Average of Levels in Binary Tree - [EASY]/README.md
@@ -0,0 +1,1 @@
+Link to the exercise : https://leetcode.com/problems/average-of-levels-in-binary-tree/description/?envType=study-plan-v2&envId=top-interview-150
---
+++ 150 INTERVIEW QUESTIONS/Binary Tree BFS/Average of Levels in Binary Tree - [EASY]/average.py
@@ -0,0 +1,46 @@
+############ INSTRUCTIONS ##############
+########################################
+
+#Given the root of a binary tree, return the average value of the nodes on each level in the form of an array. 
+#Answers within 10-5 of the actual answer will be accepted.
+
+############### EXERCISE ###############
+########################################
+
+from collections import deque
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution(object):
+    def averageOfLevels(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[float]
+        """
+        if not root:
+            return []
+
+        result = []
+        queue = deque([root])
+
+        while queue:
+            level_size = len(queue)
+            level_sum = 0.0
+
+            for _ in range(level_size):
+                node = queue.popleft()
+                level_sum += node.val
+
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+
+            result.append(level_sum / level_size)
+
+        return result
