bd46129e905bcd0e71832bab6c768f9c74c39eda
earneau <evan.arn@icloud.com>
2023-11-30 08:20:35 +0100

    Pushing exercise

---
+++ 150 INTERVIEW QUESTIONS/Math/Palindrome Number - [EASY]/palindrome.py
@@ -0,0 +1,8 @@
+############ INSTRUCTIONS ##############
+########################################
+
+#Given an integer x, return true if x is a palindrome, and false otherwise.
+
+############### EXERCISE ###############
+########################################
+
