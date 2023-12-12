dd453da38a3b9ea0981d9903b75d134658e151cd
earneau <evan.arn@icloud.com>
2023-12-12 22:18:19 +0100

    Importing solution, starting binary searches

---
+++ 150 INTERVIEW QUESTIONS/Binary Search/Search Insert Position - [EASY]/README.md
@@ -0,0 +1,1 @@
+Link to the exercise : https://leetcode.com/problems/search-insert-position/description/?envType=study-plan-v2&envId=top-interview-150
---
+++ 150 INTERVIEW QUESTIONS/Binary Search/Search Insert Position - [EASY]/search.py
@@ -0,0 +1,22 @@
+############ INSTRUCTIONS ##############
+########################################
+
+#Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+#You must write an algorithm with O(log n) runtime complexity.
+
+############### EXERCISE ###############
+########################################
+
+class Solution(object):
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        for i in range(0, len(nums)):
+            if nums[i] >= target:
+                return i
+        return len(nums)
+        
